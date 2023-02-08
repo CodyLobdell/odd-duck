15972286a4402bf3dc192f171df65a82498ac436
Cody Lobdell <thecreator85@gmail.com>
2023-02-08 04:26:07 -0800

    added local storage code

---
+++ js/localStorage.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const productArr = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dogDuck', 'dragon', 'pen', 'petSweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'waterCan', 'wineGlass'];
+localStorage.setItem('products', JSON.stringify(productArr));
+
+const productData = JSON.parse(localStorage.getItem('products'));
+console.log(productData);
+
+//const numberArr = [1, 2, 3];
+//localStorage.setItem('numbers', JSON.stringify(numberArr));
+
+//const numberData = JSON.parse(localStorage.getItem('numbers'));
+//console.log(numberData);
+
+
+//localStorage.clear();
